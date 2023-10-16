d1791054c933448af6f8049b296525aa46097ff5
AustinJohnson44 <m263126@usna.edu>
2023-10-16 09:44:37 -0400

    put flip inside while loop

--- game.py
+++ game.py
@@ -3,15 +3,14 @@ import sys
 
 pygame.init() # tells pygame to look/listen for inputs and events
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((400, 400)) # width, height
 pygame.display.set_caption("Chomp!")
 
 screen.fill((57, 165, 237)) # tuple of rgb values for color of the screen
 
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400)) # color is rgb values, rect has x location, y location, height, width
+pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400)) # color is rgb values, rect has left position, top position, width, and height
 sand = pygame.image.load("assets/images/sand.png").convert()
 screen.blit(sand, (200, 200, 64, 64))
-pygame.display.flip()
 
 while True:
     for event in pygame.event.get():
@@ -20,3 +19,5 @@ while True:
             print("Thanks for playing!")
             pygame.quit() # stops process that pygame.init started
             sys.exit() # uber break
+
+    pygame.display.flip()
