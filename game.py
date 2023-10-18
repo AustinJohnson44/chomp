594d2da699ea7523b0dfb21175c5269a626890a8
AustinJohnson44 <m263126@usna.edu>
2023-10-18 09:46:43 -0400

    Added sand tiles to bottom

--- game.py
+++ game.py
@@ -2,10 +2,10 @@ import pygame
 import sys
 
 # Dimensions
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 400
-SAND_HEIGHT = 20
 TILE_SIZE = 64  # tiles are square so height==width
+SCREEN_WIDTH = 12*TILE_SIZE
+SCREEN_HEIGHT = 8*TILE_SIZE
+SAND_HEIGHT = 20
 
 # Colors
 WATER_COLOR = (57, 165, 237)  # color is rgb values, rect has left position, top position, width, and height
@@ -21,8 +21,13 @@ screen.fill(WATER_COLOR)
 pygame.draw.rect(screen, SAND_COLOR, (0,
                                       SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()  # .convert() to make the background transparent
-screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2,
-                   SCREEN_HEIGHT/2 - TILE_SIZE/2))
+sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+sand_top.set_colorkey((0, 0, 0))  # tells blit to ignore transparent pixels and use whatever color is behind them
+
+# BLIT SAND TILES ACROSS THE BOTTOM OF THE SCREEN
+for i in range(SCREEN_WIDTH // TILE_SIZE):  # // to give quotient and not a float
+    screen.blit(sand, (TILE_SIZE * i, SCREEN_HEIGHT - TILE_SIZE))
+    screen.blit(sand_top, (TILE_SIZE * i, SCREEN_HEIGHT - TILE_SIZE*2))
 
 while True:
     for event in pygame.event.get():
