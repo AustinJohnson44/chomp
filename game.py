4fec4102f595249df489bbfdea9cded4a8310e0a
AustinJohnson44 <m263126@usna.edu>
2023-10-18 09:26:38 -0400

    Created variables for heights and widths

--- game.py
+++ game.py
@@ -1,23 +1,35 @@
 import pygame
 import sys
 
-pygame.init() # tells pygame to look/listen for inputs and events
+# Dimensions
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # tiles are square so height==width
 
-screen = pygame.display.set_mode((400, 400)) # width, height
+# Colors
+WATER_COLOR = (57, 165, 237)  # color is rgb values, rect has left position, top position, width, and height
+SAND_COLOR = (100, 25, 0)
+
+pygame.init()  # tells pygame to look/listen for inputs and events
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,
+                                  SCREEN_HEIGHT))  # collapsed variables inside parenthesis
 pygame.display.set_caption("Chomp!")
+screen.fill(WATER_COLOR)
 
-screen.fill((57, 165, 237)) # tuple of rgb values for color of the screen
-
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400)) # color is rgb values, rect has left position, top position, width, and height
-sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (200, 200, 64, 64))
+pygame.draw.rect(screen, SAND_COLOR, (0,
+                                      SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
+sand = pygame.image.load("assets/images/sand.png").convert()  # .convert() to make the background transparent
+screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2,
+                   SCREEN_HEIGHT/2 - TILE_SIZE/2))
 
 while True:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             print("Thanks for playing!")
-            pygame.quit() # stops process that pygame.init started
-            sys.exit() # uber break
+            pygame.quit()  # stops process that pygame.init started
+            sys.exit()  # uber break - breaks out of everything
 
     pygame.display.flip()
