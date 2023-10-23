e13f958bdcf281eafa299dbffc0816a564691745
AustinJohnson44 <m263126@usna.edu>
2023-10-23 09:18:48 -0400

    fish is a rectangle

--- fish.py
+++ fish.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 
 
-class Fish(pygame.sprite.Sprite):
+class Fish:
     def __init__(self, x, y):
         self.right_image = pygame.image.load("assets/images/orange_fish.png").convert()
         self.right_image.set_colorkey((0, 0, 0))
@@ -12,8 +12,7 @@ class Fish(pygame.sprite.Sprite):
 
         self.image = self.right_image
 
-        self.x = x
-        self.y = y
+        self.rect = pygame.rect.Rect(x, y, self.image.get_width(), self.image.get_height())
         self.moving_left = False
         self.moving_right = False
         self.moving_up = False
@@ -22,23 +21,23 @@ class Fish(pygame.sprite.Sprite):
     def update(self):
         if self.moving_left:
             self.image = self.left_image
-            self.x -= 2
+            self.rect.x -= 2
         elif self.moving_right:
             self.image = self.right_image
-            self.x += 2
+            self.rect.x += 2
         if self.moving_up:
-            self.y -= 2
+            self.rect.y -= 2
         elif self.moving_down:
-            self.y += 2
+            self.rect.y += 2
         # make fish stay on screen
-        if self.x < 0:
-            self.x = 0
-        elif self.x > SCREEN_WIDTH - TILE_SIZE:
-            self.x = SCREEN_WIDTH - TILE_SIZE
-        if self.y < 0:
-            self.y = 0
-        elif self.y > SCREEN_HEIGHT - TILE_SIZE*3:
-            self.y = SCREEN_HEIGHT - TILE_SIZE*3
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > SCREEN_HEIGHT - 2*TILE_SIZE:
+            self.rect.bottom = SCREEN_HEIGHT - 2*TILE_SIZE
 
     def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
